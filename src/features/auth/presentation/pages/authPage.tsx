ae2ea1eb277d445f9f722f5050ac479e4df80492
le-ar <andreiklauzer@gmail.com>
2020-06-09 03:36:33 +1000

    change authdatasource

--- src/features/auth/presentation/pages/authPage.tsx
+++ src/features/auth/presentation/pages/authPage.tsx
@@ -6,9 +6,14 @@ declare var uLogin: any;
 
 export default class AuthPage extends Component {
     componentDidMount() {
+        (window as any).preview = this.preview;
         let script = document.createElement("script");
         script.src = '//ulogin.ru/js/ulogin.js';
         document.body.appendChild(script);
+    }
+
+    async preview(token: string) {
+        console.log(token);
     }
 
     handleAuth() {
@@ -19,10 +24,10 @@ export default class AuthPage extends Component {
     render() {
         return <div>
             <div>
-                <Button onClick={() => {this.handleAuth()}}>Авторизоваться</Button>
+                <Button onClick={() => { this.handleAuth() }}>Авторизоваться</Button>
             </div>
             <div className="d-none">
-                <div id="uLogin" data-ulogin="display=small;theme=classic;fields=first_name,last_name;providers=vkontakte;hidden=;redirect_uri=http%3A%2F%2Flocalhost;mobilebuttons=0;"></div>
+                <div id="uLogin" data-ulogin="callback=preview;display=small;theme=classic;fields=first_name,last_name;providers=vkontakte;hidden=;redirect_uri=http%3A%2F%2Flocalhost;mobilebuttons=0;"></div>
             </div>
         </div>;
     }
