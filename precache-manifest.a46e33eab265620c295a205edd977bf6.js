6e52e55ea88b08739eb26fe5c65382adf76381bb
le-ar <andreiklauzer@gmail.com>
2021-07-07 02:18:00 +0300

    Updates

---
+++ precache-manifest.a46e33eab265620c295a205edd977bf6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb77f83e9ac51d23ace36826c9735661",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27865a63ca9891bab6ea",
+    "url": "/static/css/2.70422312.chunk.css"
+  },
+  {
+    "revision": "efc390b179796bd6e3c9",
+    "url": "/static/css/main.a1b1e524.chunk.css"
+  },
+  {
+    "revision": "27865a63ca9891bab6ea",
+    "url": "/static/js/2.3f12c339.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.3f12c339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf1e78dbbc292a95855e",
+    "url": "/static/js/3.909f0eb3.chunk.js"
+  },
+  {
+    "revision": "cf4267eff7974fac83db8c98abc2ba41",
+    "url": "/static/js/3.909f0eb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "397a6f10679f16cdf983",
+    "url": "/static/js/4.72517d65.chunk.js"
+  },
+  {
+    "revision": "07225e995bbec690141c",
+    "url": "/static/js/5.285a61aa.chunk.js"
+  },
+  {
+    "revision": "efc390b179796bd6e3c9",
+    "url": "/static/js/main.05360301.chunk.js"
+  },
+  {
+    "revision": "fc30184ef2b6e799a6e3",
+    "url": "/static/js/runtime-main.c898b1eb.js"
+  }
+]);
